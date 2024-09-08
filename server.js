d1a901b86ab28e3b9ef1f04c1e13f18d4daca71d
Iryna Pih <irynapih@gmail.com>
2024-09-08 11:12:48 -0400

    fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 4000;
+const port = 8000;
 
 // Mock data for users, posts, and comments
 const users = [
